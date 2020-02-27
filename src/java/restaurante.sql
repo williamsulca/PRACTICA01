f84df0a592639c93640b81c0dfe4add5eb72622f
l11m13 <l11m13@SURCO.com>
2020-02-27 16:45:01 -0500

    first commit

---
+++ src/java/restaurante.sql
@@ -0,0 +1,12 @@
+create table restaurante(
+id_restaurante int  primary key,
+nombre varchar(20),
+precio char,
+categoria varchar(20),
+
+)
+
+insert into restaurante(1,'Arroz con pollo',15,'Pollo');
+insert into restaurante(1,'Arroz chaufa',15,'Arroz');
+insert into restaurante(1,'Pollo a la norteña',15,'Pollo');
+
